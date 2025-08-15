dbfe0bbea36e9f90018b9a965857f30598118112
Gsdagustavo <gugadanielalvez@gmail.com>
2025-08-14 22:15:56 -0300

    feat: combine file readig with letter counting

--- main.go
+++ main.go
@@ -9,10 +9,8 @@ import (
 )
 
 func main() {
-	//word := flag.String("word", "default", "word that will be queried")
-	//letter := flag.String("letter", "default", "letter that will be searched for in the word")
-
 	filePath := flag.String("file", "default", "the path to the file to be queried")
+	letter := flag.String("letter", "default", "letter that will be searched for in the word")
 
 	// parse all flags
 	flag.Parse()
@@ -26,28 +24,28 @@ func main() {
 	}
 
 	// reads the file contents
-	_, err = readFile(file)
+	text, err := readFile(file)
 
 	// error during reading
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	count := countLetter(text, []rune(*letter)[0])
+
+	fmt.Printf("Occurrences of the letter %v in the file: %v", *letter, count)
+
 	_ = file.Close()
 }
 
 func readFile(file *os.File) (string, error) {
-
 	bytes, err := io.ReadAll(io.Reader(file))
 
 	if err != nil {
 		return "", err
 	}
 
-	text := string(bytes)
-	fmt.Println("File content: " + text)
-
-	return text, nil
+	return string(bytes), nil
 }
 
 func countLetter(str string, letter rune) int {
